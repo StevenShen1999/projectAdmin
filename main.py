32bb1492ad780b05be7d354fd08b60e50c660fc1
steven.shen <steven.shen@bytedance.com>
2024-12-06 15:22:37 +1100

    feat: add Checker logic

---
+++ main.py
@@ -0,0 +1,49 @@
+from openpyxl import load_workbook, Workbook
+
+
+class AdmissionDiffChecker:
+    def __init__(self):
+        self.old_admission_map = set()
+        self.header_row = None
+        self.diff_rows = []
+
+    def load_older_worksheet(self):
+        wb = load_workbook(filename="assets/example_sheet_A.xlsx")
+        ws = wb.active
+        for row in ws.iter_rows(min_row=1, values_only=True):
+            # Write header row for result
+            if not self.header_row:
+                self.header_row = row
+                continue
+            if not row[0] or not row[1]:
+                continue
+            self.old_admission_map.add(row[0] + row[1])
+
+    def load_and_compare_newer_worksheet(self):
+        wb = load_workbook(filename="assets/example_sheet_B.xlsx")
+        ws = wb.active
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            if not row[0] or not row[1]:
+                continue
+            name = row[0] + row[1]
+            if name not in self.old_admission_map:
+                self.diff_rows.append(row)
+            # This is for de-duplication
+            self.old_admission_map.add(name)
+
+    def write_result_to_new_worksheet(self):
+        wb = Workbook()
+        ws = wb.active
+        ws.append(checker.header_row)
+        for row in self.diff_rows:
+            ws.append(row)
+        wb.save("assets/admin_diff.xlsx")
+
+    def do_comparison(self):
+        self.load_older_worksheet()
+        self.load_and_compare_newer_worksheet()
+        self.write_result_to_new_worksheet()
+
+
+checker = AdmissionDiffChecker()
+checker.do_comparison()
