80998c1ef8db3f24872a6e9229aea7a207358d8b
steven.shen <steven.shen@bytedance.com>
2024-12-06 15:26:47 +1100

    feat: add logs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook, Workbook
+from datetime import datetime
 
 
 class AdmissionDiffChecker:
@@ -8,6 +9,7 @@ class AdmissionDiffChecker:
         self.diff_rows = []
 
     def load_older_worksheet(self):
+        print("Parsing Older Worksheet")
         wb = load_workbook(filename="assets/example_sheet_A.xlsx")
         ws = wb.active
         for row in ws.iter_rows(min_row=1, values_only=True):
@@ -20,6 +22,7 @@ class AdmissionDiffChecker:
             self.old_admission_map.add(row[0] + row[1])
 
     def load_and_compare_newer_worksheet(self):
+        print("Parsing Newer Worksheet")
         wb = load_workbook(filename="assets/example_sheet_B.xlsx")
         ws = wb.active
         for row in ws.iter_rows(min_row=2, values_only=True):
@@ -45,5 +48,9 @@ class AdmissionDiffChecker:
         self.write_result_to_new_worksheet()
 
 
+start_time = datetime.now()
+print(f"Begun Checking Diffs {start_time}")
 checker = AdmissionDiffChecker()
 checker.do_comparison()
+finish_time = datetime.now() - start_time
+print(f"Finished Checking Diffs. Time Taken: {finish_time.microseconds} microseconds")
